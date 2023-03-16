61b784eef36e60e2656af129ec7cc16b4df29a60
Alexey Gaidykov <swiftgaida@list.ru>
2023-03-16 19:07:28 +0300

    .hyz

--- TetsAppIn/ViewController.swift
+++ TetsAppIn/ViewController.swift
@@ -5,15 +5,34 @@
 //  Created by Alexey Gaidykov on 16.03.2023.
 //
 
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initilazet()
     }
-
-
+    
+    private func initilazet() {
+        view.backgroundColor = .systemBackground
+        
+        let button = UIButton(type: .system)
+        button.setTitle("Click me 𖫪", for: .normal)
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(50)
+        }
+        
+        let label = UILabel()
+        label.text = "Welcome"
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(20)
+            make.top.equalToSuperview().inset(150)
+        }
+    }
 }
 
